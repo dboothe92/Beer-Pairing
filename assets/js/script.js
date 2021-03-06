4fdfc25bacc8e688fd7882c2e9dbebd4f493b6ce
Dan B <dbraunby@hotmail.com>
2021-03-06 09:55:36 -0700

    initial html

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,22 +3,27 @@ const ctx = canvas.getContext("2d");
 const scale = 10;
 const rows = canvas.height / scale;
 const columns = canvas.width / scale;
-var snake;
+let snake;
 
+//Handles where the snake is drawn and movement
 (function setup() {
-snake = new snake();
+    snake = new Snake();
+    fruit = new Fruit();
 
-window.setInterval(()=> {
-    ctx.clearRect(0,0, canvas.width, canvas.height)
-    snake.update();
-    snake.draw();
+    fruit.pickLocation();
+
+    window.setInterval(()=> {
+        ctx.clearRect(0,0, canvas.width, canvas.height)
+        fruit.draw();
+        snake.update();
+        snake.draw();
+        
     
-}, 250);
-
-
+    }, 250);
 }());
+
+//adds ability to cvontrol snake with arrow keys
 window.addEventListener('keydown',((evt) => {
-   
     const direction = evt.key.replace('Arrow', '');
     snake.changeDirection(direction);
-}))
+}));
--- assets/js/snake.js
+++ assets/js/snake.js
@@ -1,21 +1,32 @@
-function snake () {
+//function that controls movement of snake 
+function Snake () {
+    //Starting point
     this.x = 0;
     this.y= 0;
     this.xSpeed = scale * 1;
     this.ySpeed = 0;
+    // fills rectangle
     this.draw = function () {
         ctx.fillStyle = "#ffe120";
         ctx.fillRect(this.x, this.y, scale, scale);
     }
+
+    //checks if snake is leaving the canvas
    this.update = function () {
-   this.x += this.xSpeed;
-   this.y += this.ySpeed;
-   if (this.x > canvas.width) {
-       this.x = 0
-   }
- 
- 
-   }
+    this.x += this.xSpeed;
+    this.y += this.ySpeed;
+    if (this.x > canvas.width) {
+        this.x = 0
+    } if (this.y > canvas.height) {
+      this.y = 0.
+    } if (this.x < 0) {
+      this.x = canvas.width
+    } if (this.y < 0) {
+      this.y = canvas.height
+    }
+  };
+
+  //Checks direction of snake and adjusts movement
    this.changeDirection = function(direction) {
       switch(direction) {
         case "Up":
@@ -34,9 +45,7 @@ function snake () {
           this.xSpeed = scale * 1;
           this.ySpeed = 0;
           break;
-           
- 
-      }
-    }
- }
+      };
+    };
+ };
  
